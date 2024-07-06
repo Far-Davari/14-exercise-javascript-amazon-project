6a45c1e4da71ab9b7c08bd7b43abcdaf9f50ab95
Farshad Davari <DavariFarshad3@outlook.com>
2024-07-06 16:24:15 +0330

    Add keyup eventListener to input field.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -45,7 +45,7 @@ cart.forEach(cartItem => {
               data-product-id=${matchingProduct.id}>
               Update
             </span>
-            <input class="quantity-input js-quantity-input-${matchingProduct.id}" />
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}" data-product-id=${matchingProduct.id} />
             <span class="save-quantity-link link-primary js-save-link" data-product-id=${matchingProduct.id}>
               Save
             </span>
@@ -170,5 +170,36 @@ document.querySelectorAll(".js-update-link")
         quantityLabel.innerHTML = newQauntity;
 
         updateCartQuantity();
+      })
+    })
+
+document.querySelectorAll(".quantity-input")
+    .forEach(input => {
+      input.addEventListener("keydown", (event) => {
+        if(event.key === "Enter") {
+          const { productId } = input.dataset;
+
+          const container = document.querySelector(
+            `.js-cart-item-container-${productId}`
+          );
+          container.classList.remove("is-editing-quantity");
+
+          const quantityInput = document.querySelector(`.js-quantity-input-${productId}`).value;
+          const newQauntity = Number(quantityInput);
+        
+          if (newQauntity < 0 || newQauntity >= 1000) {
+            alert("Quantity must be at least 0 and less than 1000.");
+            return;
+          }
+  
+          updateQuantity(productId, newQauntity);
+
+          const quantityLabel = document.querySelector(
+            `.js-quantity-label-${productId}`
+          );
+          quantityLabel.innerHTML = newQauntity;
+
+          updateCartQuantity();
+        }
       })
     })
